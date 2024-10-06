fd60beba8232974835e4fdcf8101895de3fd358e
ankit yadav <ankit6686482@gmail.com>
2024-10-06 20:45:13 +0530

    album & playlist activated

--- components/searchResults.js
+++ components/searchResults.js
@@ -2,14 +2,39 @@ import React from "react";
 import MusicList from "./MusicList";
 import { ScrollView } from "react-native";
 import { Text, View } from "./Themed";
+import { useDispatch } from "react-redux";
+import { loadAndPlayTrack } from "@/state/slices/audio.slice";
+import { useRouter } from "expo-router";
 
 const SearchResults = ({ results }) => {
+
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const handlePress = (item) => {
+    const isAlbum = item.resultType==='album';
+    const isPlaylist = item.resultType==='playlist';
+    const isArtist = item.resultType==='artist';
+    if(item.videoId){
+      dispatch(loadAndPlayTrack({track: item}))
+    }
+    else if(isAlbum || isPlaylist){
+      router.push({
+        pathname: "/(tabs)/homeRoutes/Album",
+        params: { id: item.browseId, isPlaylist: isPlaylist }, 
+      });
+    }
+    else{
+      console.log('not implemented')
+    }
+  }
+
   return (
     <ScrollView>
       {results?.map((item, index) => (
         <View key={index}>
           <Text style={{fontSize: 20, fontWeight: 'bold', padding: 10}}>{item.category}</Text>
-          <MusicList dataset={item.data} />
+          <MusicList dataset={item.data} onPress={handlePress} />
         </View>
       ))}
     </ScrollView>
--- services/album.service.js
+++ services/album.service.js
@@ -1,8 +1,8 @@
 import musicApi from "../configs/apiClient.config";
 
-export const getAlbum = async (id) => {
+export const getAlbum = async (id, isPlaylist) => {
   try {
-    const response = await musicApi.get(`/albuminfo/${id}`);
+    const response = await musicApi.get(`/${isPlaylist==='true'?'playlist':'albuminfo'}/${id}`);
     return response.data;
   } catch (error) {
     console.error("Error fetching album info:", error);
